df963e90eabfba722d9c81c9f8c5f9c3c1b25531
Joshua Cantero <cantero.joshua@gmail.com>
2017-04-11 22:18:47 -0700

    Set up google map, tab layouts attached with a viewpager.

--- ParkingProject/app/src/main/java/com/example/bruhshua/parkingproject/MainActivity.java
+++ ParkingProject/app/src/main/java/com/example/bruhshua/parkingproject/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.bruhshua.parkingproject;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TabLayout mTabLayout;
+    private ViewPagerAdapter mAdapter;
+    private ViewPager mViewPager;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        mAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.viewPager);
+        mViewPager.setAdapter(mAdapter);
+
+        mTabLayout.setupWithViewPager(mViewPager);
+
     }
+
+
+
+
 }
